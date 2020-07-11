8dc2a2f136a0cdf3050260388af43c15186c2efc
vaneycksr <vaneyck23@gmail.com>
2020-07-10 23:13:15 -0300

    criaao da classe de dominio do user e implementacao dos testes de usuario especifico, usuario nao encontrado e criacao de usuario

--- src/test/java/domain/User.java
+++ src/test/java/domain/User.java
@@ -1,8 +1,25 @@
 package domain;
 
-// TODO: 09/07/2020 criar os atributos com o lombok 
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-    
-    
+
+    // atributos que podem vim no JSON
+    private Integer id;
+    private String email;
+    @JsonAlias("name")
+    private String first_name;
+    private String last_name;
+    private String avatar;
+    private String job;
     
 }
--- src/test/java/tests/UsersTest.java
+++ src/test/java/tests/UsersTest.java
@@ -1,17 +1,20 @@
 package tests;
 
+import domain.User;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
 import support.BaseTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class UsersTest extends BaseTest {
 
-    private static final String LIST_USERS_ENDPOINT = "/users";
+    private static final String USERS_ENDPOINT = "/users";
     private static final String SINGLE_USER_ENDPOINT = "/users/{userId}";
 
 
@@ -20,10 +23,8 @@ public class UsersTest extends BaseTest {
 
         given().
                 param("page","2").
-
         when().
-                get(LIST_USERS_ENDPOINT).
-
+                get(USERS_ENDPOINT).
         then().
                 statusCode(HttpStatus.SC_OK).
                 body("page", is(2)).
@@ -34,16 +35,60 @@ public class UsersTest extends BaseTest {
     @Test
     public void testValidarUsuarioEspecifico(){
 
-        given().
+        User user = given().
                 pathParam("userId",2).
+        when().
+                get(SINGLE_USER_ENDPOINT).
+        then().
+                statusCode(HttpStatus.SC_OK).
+
+        //  na resposta do get, pega o campo data, e passa como parametro a classe na qual tem os atributos
+        extract().
+                body().jsonPath().getObject("data",User.class);
+
+        assertThat(user.getEmail(),containsString("janet.weaver@"));
+        assertThat(user.getFirst_name(),is("Janet"));
+        assertThat(user.getLast_name(),is("Weaver"));
+
+    }
+
+    @Test
+    public void testUsuarioNaoEncontrado(){
+
+        given().
+                pathParam("userId",50).
 
         when().
                 get(SINGLE_USER_ENDPOINT).
 
         then().
-                statusCode(HttpStatus.SC_OK);
+                statusCode(HttpStatus.SC_NOT_FOUND).
+                body(equalTo("{}"));
 
+    }
 
+    @Test
+    public void testCriarUsuario(){
+
+        Map<String, String> user = new HashMap<>();
+        user.put("name","vaneyck");
+        user.put("job","QA");
+
+        given().
+                body(user).
+        when().
+                post(USERS_ENDPOINT).
+        then().
+                statusCode(HttpStatus.SC_CREATED).
+                body("name", is("vaneyck")).
+                body("job", is("QA")).
+                body("createdAt", is(notNullValue()));
+
+    }
+
+    // TODO implementar testes do metodo UPDATE
+    @Test
+    public void testEditarUsuario(){
 
     }
 
